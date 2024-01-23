35469ff374ef1aabbfcceb160472f17374764625
mahi13singh2004 <mahi13singh2004@gmail.com>
2024-01-23 17:36:37 +0530

    Create LargestElement.cpp

---
+++ Arrays/LargestElement.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> 
+int largestElement(vector<int>&array, int N) {
+    int largest=INT_MIN;
+    for(int i=0; i<N; i++){
+        if(array[i]>largest){
+            largest=array[i];
+        }
+    }
+    return largest;
+}
