32904b6d7e8284cbb83fa4dee832e0497a3fe037
Mahi Singh <154801399+mahi13singh2004@users.noreply.github.com>
2024-03-05 15:11:01 +0530

    Create linear_search.cpp

---
+++ Arrays/linear_search.cpp
@@ -0,0 +1,12 @@
+int linearSearch(int n, int num, vector<int> &arr)
+{
+    int answer=-1;
+
+    for(int i=0; i<n; i++){
+        if(arr[i]==num){
+            answer=i;
+            break;
+        }
+    }
+    return answer;
+}
