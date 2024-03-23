00f4a5624d930933d9ce3e9d17fc7cefa0b3db0b
Mahi Singh <154801399+mahi13singh2004@users.noreply.github.com>
2024-03-23 19:01:32 +0530

    Update sort_0_1_2.cpp

--- Arrays/sort_0_1_2.cpp
+++ Arrays/sort_0_1_2.cpp
@@ -19,3 +19,37 @@ void sortArray(vector<int>& array, int n)
         }
     }
 }
+
+
+
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+  long long count0=0;
+  long long count1=0;
+  long long count2=0;
+
+  for(int i=0; i<n; i++){
+     if(arr[i]==0){
+        count0++;
+     }
+     else if(arr[i]==1){
+        count1++;
+     }
+     else{
+        count2++;
+     }
+  }
+
+  for(int i=0; i<count0; i++){
+     arr[i]=0;
+  }
+
+  for(int i=count0; i<count0+count1; i++){
+     arr[i]=1;
+  }
+
+  for(int i=count0+count1; i<n; i++){
+     arr[i]=2;
+  }
+}
